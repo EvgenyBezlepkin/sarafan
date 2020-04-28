6d9036c08a6aafe85278f614abc76fd3737317bb
Evgeny <evgeny-bezlepkin@mail.ru>
2020-04-28 15:46:13 +0700

    работа с базой из vue

--- src/main/java/letscode/sarafan/controller/A.java
+++ src/main/java/letscode/sarafan/controller/A.java
@@ -3,6 +3,7 @@ package letscode.sarafan.controller;
 import letscode.sarafan.domain.Message;
 import letscode.sarafan.exceptions.NotFoundException;
 import letscode.sarafan.repository.MessageRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,29 +27,20 @@ public class A {
     }
 
     @PostMapping
-    public Map<String, String> putMessage(@RequestBody Message message) {
+    public Message putMessage(@RequestBody Message message) {
         messageRepository.save(message);
         return message;
     }
 
     @PutMapping("{id}")
-    public Map<String, String> updateMessage(@PathVariable String id, @RequestBody Map<String, String> message){
-        Map<String, String> getMessage = findMessage(id);
-        getMessage.put("text", message.get("text"));
-        return getMessage;
+    public Message updateMessage(@PathVariable("id") Message messagefromDb, @RequestBody Message message){
+        BeanUtils.copyProperties(message, messagefromDb, "id");
+        return messageRepository.save(messagefromDb);
     }
 
     @DeleteMapping("{id}")
-    public void deleteMessage(@PathVariable String id) {
-        Map<String, String> message = findMessage(id);
-        messages.remove(message);
+    public void deleteMessage(@PathVariable("id") Message message) {
+        messageRepository.delete(message);
     }
 
-    private Map<String, String> findMessage(@PathVariable String id) {
-        return messages
-                .stream()
-                .filter(message -> message.get("id").equals(id))
-                .findFirst()
-                .orElseThrow(NotFoundException::new);
-    }
 }
